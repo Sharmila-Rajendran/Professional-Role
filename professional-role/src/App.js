ece07ecbd08aa49924eaa38bfab18e5adbed1958
Sharmila-Rajendran <sharmila300803@gmail.com>
2023-10-28 07:38:23 +0530

    Professional-Role

---
+++ professional-role/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Role from "./Components/Role";
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Role/>
+    </div>
+  )
+}
+
+export default App;
